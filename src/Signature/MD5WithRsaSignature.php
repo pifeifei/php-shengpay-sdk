7f6e8cae52f45b4cb893de454fcaa6d2829855c9
kis龍 <pifeifei1989@qq.com>
2020-08-21 15:06:13 +0800

    modify MD5WithRsaSignature::method return to RSA

--- src/Signature/MD5WithRsaSignature.php
+++ src/Signature/MD5WithRsaSignature.php
@@ -11,7 +11,7 @@ class MD5WithRsaSignature extends Signature implements SignatureInterface
      */
     public function getMethod()
     {
-        return 'MD5withRSA';
+        return 'RSA';
     }
 
     /**
